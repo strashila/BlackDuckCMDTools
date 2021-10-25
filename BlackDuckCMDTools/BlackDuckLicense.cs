36a5b03a4558e8cae28b9cdc7074ad529c298f87
Alex Knurenko <knurenko@synopsys.com>
2021-10-25 11:36:03 +0300

    Added BlackDuckLicense

---
+++ BlackDuckCMDTools/BlackDuckLicense.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackDuckCMDTools
+{
+    public class BlackDuckLicense
+    {
+        public string name;
+        public List<string> licenseFamily;
+        public string ownership;
+        public string licenseStatus;
+        public DateTime expirationDate;
+        public BlackDuckAPI_meta _meta;
+
+    }
+}
